10b1ba05a6a56fa4b650235879341bbe4066582e
Jesse Shawl <jesse@jesse.sh>
2024-05-05 16:11:10 -0500

    remove version info

--- e2e_test.go
+++ e2e_test.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"os/exec"
 	"strings"
 	"testing"
 )
 
 func TestNoArguments(t *testing.T) {
-	version, _ := exec.Command("tree", "--version").Output()
-	fmt.Println(string(version))
 	out, _ := exec.Command("tree", "test").Output()
 	expected := strings.ReplaceAll(string(out), "\u00a0", " ")
 	actual := entrypoint()
