044757e46e49a02162e77542c32fe92d2e3081db
Jesse Shawl <jesse@jesse.sh>
2024-05-05 14:21:56 -0500

    add subdirectories (#5)

--- main.go
+++ main.go
@@ -1,67 +1,96 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
+	"slices"
 	"strings"
 )
 
 type NodeType string
 
 type Node struct {
-  Children []*Node
-  Name string
-  Type NodeType
+	Children []*Node
+	Name     string
+	Type     NodeType
 }
 
 const (
-	File   NodeType = "file"
+	File      NodeType = "file"
 	Directory NodeType = "directory"
 )
 
-func main() {
-	file1 := Node{Name: "file1"}
-	file2 := Node{Name: "file2"}
-	node := Node{
-		Name: ".",
-		Children: []*Node{&file1, &file2},
-	}
-	fmt.Print(render(node))
+type Counter struct {
+	Files       int
+	Directories int
 }
 
-func summary(numFiles int, numDirectories int) string {
+func main() {
+
+}
+
+func summary(numDirectories int, numFiles int) string {
 	directories := ""
 	files := fmt.Sprintf("%d files", numFiles)
-	if numFiles > 0 {
-		directories = fmt.Sprintf("%d directory", 1)
+
+	if numFiles == 0 {
+		numDirectories = 0
+	}
+
+	if numDirectories == 1 {
+		directories = fmt.Sprintf("%d directory", numDirectories)
 	} else {
 		directories = fmt.Sprintf("%d directories", numDirectories)
 	}
 	return fmt.Sprintf("%s, %s", directories, files)
 }
 
-func tree(node Node) string {
+func indent(s string, depth int) string {
+	var line strings.Builder
+	for i := 0; i < depth; i++ {
+		line.WriteString("│   ")
+	}
+	line.WriteString(s)
+	return line.String()
+}
+
+func tree(node Node, depth int, counter *Counter) string {
 	numFiles := len(node.Children)
+
+	slices.SortFunc(node.Children, func(a, b *Node) int {
+		return cmp.Compare(a.Type, b.Type)
+	})
+
 	var list strings.Builder
-    for i := 0; i < numFiles; i++ {
-		if i == numFiles - 1 {
-			list.WriteString("└── ")
+	for i := 0; i < numFiles; i++ {
+		if i == numFiles-1 {
+			list.WriteString(indent("└── ", depth))
 		} else {
-			list.WriteString("├── ")
+			list.WriteString(indent("├── ", depth))
 		}
-        list.WriteString(node.Children[i].Name)
+		list.WriteString(node.Children[i].Name)
 		list.WriteString("\n")
-    }
+		if node.Children[i].Type == Directory {
+			counter.Directories++
+			list.WriteString(tree(*node.Children[i], depth+1, counter))
+		} else {
+			counter.Files++
+		}
+	}
+	if depth == 0 {
+		list.WriteString("\n")
+		list.WriteString(summary(counter.Directories, counter.Files))
+		list.WriteString("\n")
+	}
 	return list.String()
 }
 
 func render(node Node) string {
-	numFiles := len(node.Children)
-	numDirectories := len(node.Children)
+	counter := &Counter{Files: 0, Directories: 1}
 
 	return fmt.Sprintf(
-		"%s\n%s\n%s\n", 
-		node.Name, 
-		tree(node), 
-		summary(numFiles, numDirectories),
+		"%s\n%s",
+		node.Name,
+		tree(node, 0, counter),
 	)
 }
--- main_test.go
+++ main_test.go
@@ -4,13 +4,14 @@ import (
 	"testing"
 )
 
-func TestRenderEmptyDirectory(t *testing.T){
+func TestRenderEmptyDirectory(t *testing.T) {
 	expected := `.
 
 0 directories, 0 files
 `
-    node := Node{
+	node := Node{
 		Name: ".",
+		Type: Directory,
 	}
 	actual := render(node)
 	if actual != expected {
@@ -25,13 +26,42 @@ func TestRenderDirectoryWithFiles(t *testing.T) {
 
 1 directory, 2 files
 `
-    file1 := Node{Name: "file1"}
-	file2 := Node{Name: "file2"}
+	file1 := Node{Name: "file1", Type: File}
+	file2 := Node{Name: "file2", Type: File}
 	node := Node{
-		Name: ".",
+		Name:     ".",
 		Children: []*Node{&file1, &file2},
+		Type:     Directory,
 	}
-    actual := render(node)
+	actual := render(node)
+	if actual != expected {
+		t.Errorf("Expected:\n%s, Actual:\n%s", expected, actual)
+	}
+}
+
+func TestRenderDirectoryWithSubdirectories(t *testing.T) {
+	expected := `.
+├── directory1
+│   ├── directory2
+│   │   └── file4
+│   └── file3
+├── file1
+└── file2
+
+3 directories, 4 files
+`
+	file1 := Node{Name: "file1", Type: File}
+	file2 := Node{Name: "file2", Type: File}
+	file3 := Node{Name: "file3", Type: File}
+	file4 := Node{Name: "file4", Type: File}
+	directory2 := Node{Name: "directory2", Type: Directory, Children: []*Node{&file4}}
+	directory1 := Node{Name: "directory1", Type: Directory, Children: []*Node{&file3, &directory2}}
+	node := Node{
+		Name:     ".",
+		Children: []*Node{&file1, &file2, &directory1},
+		Type:     Directory,
+	}
+	actual := render(node)
 	if actual != expected {
 		t.Errorf("Expected:\n%s, Actual:\n%s", expected, actual)
 	}
