3575868a32c9bfebc69d2ca3a5006ceac376a7e5
Jesse Shawl <jesse@jesse.sh>
2024-05-05 12:34:52 -0500

    add file listing

--- main.go
+++ main.go
@@ -1,19 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-type Tree struct {
-  Directories []Tree
-  Files []string
+type NodeType string
+
+type Node struct {
+  Children []*Node
+  Name string
+  Type NodeType
 }
+
+const (
+	File   NodeType = "file"
+	Directory NodeType = "directory"
+)
 
 func main() {
-	tree := Tree {}
-	fmt.Print(render(tree))
+	file1 := Node{Name: "file1"}
+	file2 := Node{Name: "file2"}
+	node := Node{
+		Name: ".",
+		Children: []*Node{&file1, &file2},
+	}
+	fmt.Print(render(node))
 }
 
-func render(tree Tree) string {
-	directories := len(tree.Directories)
-	files := len(tree.Files)
-	return fmt.Sprintf(".\n\n%d directories, %d files\n", directories, files)
+func render(node Node) string {
+	numFiles := len(node.Children)
+	directories := ""
+	if numFiles > 0 {
+		directories = fmt.Sprintf("%d directory", 1)
+	} else {
+		directories = fmt.Sprintf("%d directories", 0)
+	}
+	var list strings.Builder
+
+    for i := 0; i < numFiles; i++ {
+		if i == numFiles - 1 {
+			list.WriteString("└── ")
+		} else {
+			list.WriteString("├── ")
+		}
+        list.WriteString(node.Children[i].Name)
+		list.WriteString("\n")
+    }
+
+	name := node.Name
+	return fmt.Sprintf("%s\n%s\n%s, %d files\n", name, list.String(), directories, numFiles)
 }
--- main_test.go
+++ main_test.go
@@ -9,8 +9,30 @@ func TestRenderEmptyDirectory(t *testing.T){
 
 0 directories, 0 files
 `
-	actual := render(Tree{})
+    node := Node{
+		Name: ".",
+	}
+	actual := render(node)
 	if actual != expected {
 		t.Errorf("Expected: %s, Actual: %s", expected, actual)
+	}
+}
+
+func TestRenderDirectoryWithFiles(t *testing.T) {
+	expected := `.
+├── file1
+└── file2
+
+1 directory, 2 files
+`
+    file1 := Node{Name: "file1"}
+	file2 := Node{Name: "file2"}
+	node := Node{
+		Name: ".",
+		Children: []*Node{&file1, &file2},
+	}
+    actual := render(node)
+	if actual != expected {
+		t.Errorf("Expected:\n%s, Actual:\n%s", expected, actual)
 	}
 }
