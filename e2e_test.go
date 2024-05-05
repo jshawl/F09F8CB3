e554ffe4356e44b1bd2aecc24769d640db2277b8
Jesse Shawl <jesse@jesse.sh>
2024-05-05 16:41:47 -0500

    show hidden files with -a

--- e2e_test.go
+++ e2e_test.go
@@ -9,7 +9,16 @@ import (
 func TestNoArguments(t *testing.T) {
 	out, _ := exec.Command("tree", "test").Output()
 	expected := strings.ReplaceAll(string(out), "\u00a0", " ")
-	actual := entrypoint()
+	actual := entrypoint("test", Options{all: false})
+	if actual != expected {
+		t.Errorf("Expected: %s, Actual: %s", expected, actual)
+	}
+}
+
+func TestOptionAll(t *testing.T) {
+	out, _ := exec.Command("tree", "test", "-a").Output()
+	expected := strings.ReplaceAll(string(out), "\u00a0", " ")
+	actual := entrypoint("test", Options{all: true})
 	if actual != expected {
 		t.Errorf("Expected: %s, Actual: %s", expected, actual)
 	}
